bd787fe851a78e89dbbc3627e984674cf72c864d
mahachaouch <maha-chaouch@hotmail.fr>
2016-01-16 23:22:48 +0100

    forcer le commit

--- bataille_navale_olajos_ridderstrom(1)/ListeBateau.c
+++ bataille_navale_olajos_ridderstrom(1)/ListeBateau.c
@@ -5,6 +5,36 @@
 
 TListBateau creer_listBateau()
 {
+	TListBateau l=NULL;
+	return l ;
 
-	
 }
+
+int EstVide(TListBateau l)
+{
+return (l== NULL);
+
+}
+
+TListBateau add_bateau(TListBateau l, TBateau bateau);
+{
+
+TListBateau l1= malloc(sizeof(ListBateau));
+if (EstVide(l)) 
+{
+	
+	l1->element= bateau;
+	l1->suivant=NULL;
+	l1->precedent=NULL;
+ 
+}
+else 
+{
+	l1->element = bateau ;
+	l1->suivant=l;
+	l1->precedent=NULL;
+	l->precedent=l1;
+}
+return l1 ;
+}
+
