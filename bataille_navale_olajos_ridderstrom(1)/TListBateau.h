80f8bcefe6c64cd00777622b7e8b4c493d9513ce
mahachaouch <maha-chaouch@hotmail.fr>
2016-01-16 23:24:21 +0100

    correction

--- bataille_navale_olajos_ridderstrom(1)/TListBateau.h
+++ bataille_navale_olajos_ridderstrom(1)/TListBateau.h
@@ -7,9 +7,11 @@
 
 typedef struct ListBateau ListBateau;
 
-struct LisBateau
+struct ListBateau
 {
-	TBateau bateau ;
+	Bateau* element ;
+	Bateau* suivant ;
+	Bateau* precedent;
 	
 
 };
@@ -26,10 +28,21 @@ typedef ListBateau* TListBateau;
 /* --------- cette fonction n est pas utilisee dans le main = pas d interet a la coder --------- */
 
 TListBateau creer_listBateau();
-TListBateau add_bateau(TListBateau list, TBateau bateau, TJoueur j);
+int EstVide(TListBateau l);/*renvoie 1 si la liste est vide , 0 sinon*/
+TListBateau add_bateau(TListBateau list, TBateau bateau); /*ajoute un bateau a la liste des bateaux*/
+
 TListBateau supprimer_bateau(TJoueur joueur, TBateau bateau, int i);
+/* cette fonction n est pas utilise dans le main = 
+pas d interet a la coder 
+d autant plus,elle est n a pas de specifications*/
+
 TBateau get_bateau(TJoueur joueur, TListBateau bateau, int i);
+/* meme chose pour cette fonction*/
+
 TListBateau rendre_touche(TJoueur j, TBateau bat); /*rendre bateau touché*/
 TListBateau rendre_coule(TJoueur j, TBateau bat); /*rendre bateau couleé*/
+/* comment ? les specifications de joueur et bateau ne donnent aucun indice 
+cette fonction n est pas utilisee dans le main
+le main indique que :lorsqu un bateau est touche il est copie est inserer dans une liste "touches"*/
 
 #endif 
